bd2abe1a11e146b6e054b89813da6553f5545f59
Luiz Gustavo <luizgustavorosa@outlook.com>
2022-10-29 18:27:30 -0300

    add domain

---
+++ src/Domain/Entities/NamedAPIResource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class NamedAPIResource
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class PokemonListItemViewModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ src/Domain/Entities/NamedAPIResourceList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class NamedAPIResourceList
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public NamedAPIResource[] Results { get; set; }
+    }
+
+    public class PokemonListViewModel
+    {
+        public int Count { get; set; }
+        public IEnumerable<PokemonListItemViewModel> Pokemons { get; set; }
+    }
+}
