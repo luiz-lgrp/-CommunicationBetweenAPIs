fff6302f3fbb71034c3c8b16eed3a87f2db42f4c
Luiz Gustavo <luizgustavorosa@outlook.com>
2022-10-31 14:29:58 -0300

    add communication with refit

---
+++ src/PokeAcademy.Refit/Services/IPokeService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using PokeAcademy.Refit.Controllers;
+using Refit;
+
+namespace PokeAcademy.Refit.Services
+{
+    public interface IPokeService
+    {
+
+        [Get("/pokemon")]
+        Task<NamedAPIResourceList> GetAll(int limit);
+
+        [Get("/pokemon/{id}")]
+        Task<Root> GetById(int id);
+    }
+
+}
